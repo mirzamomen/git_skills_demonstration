5571b2ecc0b943f4f86ed0ec5d9cbd7eac694556
zmir0004 <zahra.mirzamomen@monash.edu>
2026-07-20 22:04:02 +1000

    Discount coffee to 3.25 for a promotion

--- store.py
+++ store.py
@@ -12,7 +12,7 @@ OPENING_HOURS = "9am - 6pm"
 
 # Each entry is "product": price (in USD).
 MENU = {
-    "coffee": 3.50,
+    "coffee": 3.25,
     "tea": 3.00,
     "bagel": 2.50,
     # Add new products here
